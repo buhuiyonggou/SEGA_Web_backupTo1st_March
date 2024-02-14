bb094dfd458dbb45e214cf0f4e828e5ce9e8ffff
Yucheng Wang <wang.yucheng1@northeastern.edu>
2024-02-14 13:07:08 -0800

    Add algorithm choose page

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def upload_file():
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
-            return redirect(url_for('network_graph', filename=filename))
+            return render_template('analyze.html', filename=filename)
     return render_template('upload.html')
 
 
